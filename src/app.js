d23ce22057ac02e63c7d765a9b6894df58b6475a
hlkhala022 <hlkhala022@student.wethinkcode.co.za>
2023-09-07 14:29:00 +0200

    displayed the forecast data using the api

--- src/app.js
+++ src/app.js
@@ -22,32 +22,47 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function dailyForecast(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    
+    let days = ["Sun","Mon","Tue","Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response){
+
+    // console.log(response.data.daily);
+    let daysForecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
     
-    let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
     
     let forecastHTML = `<div class="row">`;
 
-    days.forEach(function(day) {
-        
-        forecastHTML = forecastHTML +  `
-        
-            <div class="col-2">
-                <div class="weather-forecast-date">
-                    ${day}
+    daysForecast.forEach(function (forecastDay, index) {
+
+        if (index < 6){
+
+            forecastHTML = forecastHTML +  `
+            
+                <div class="col-2">
+                    <div class="weather-forecast-date">
+                        ${dailyForecast(forecastDay.dt)}
+                    </div>
+                    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max">
+                            ${Math.round(forecastDay.temp.max)}°
+                        </span>
+                        <span class="weather-forecast-temperature-min">
+                            ${Math.round(forecastDay.temp.min)}°
+                        </span>
+                    </div>
                 </div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42">
-                <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max">
-                        18°
-                    </span>
-                    <span class="weather-forecast-temperature-min">
-                        12°
-                    </span>
-                </div>
-            </div>
-        `;
+            `;
+        }
     });
     forecastHTML = forecastHTML + `</div>`;  
     forecastElement.innerHTML = forecastHTML;
@@ -93,6 +108,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
+
+
 }
 
 function search(city) {
@@ -144,5 +161,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Johannesburg");
-
-displayForecast();
